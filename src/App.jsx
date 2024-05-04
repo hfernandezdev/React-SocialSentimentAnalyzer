4de2b109e2fdc2f1df8a977eb6eeebc421f39dd1
Humberto Fernández <humbertof44@gmail.com>
2024-05-04 19:28:04 -0400

    Implement authentication system using Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import LoginButton from './components/Login'
+import Profile from './components/Profile'
+import LogoutButton from './components/Logout'
 import './App.css'
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <>
       <h1>React-SocialSentimentAnalyzer</h1>
+      {isAuthenticated ? <>
+        <div className="card">
+          <Profile />
+          <LogoutButton />
+        </div>
+      </> : <>
+        <LoginButton />
+      </>}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { Auth0Provider } from '@auth0/auth0-react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+        domain="dev-0rlkhqm60x2f0vya.us.auth0.com"
+        clientId="VYbZog6s3Ecwj8DPjQW0h11yVpB6PK20"
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+      >
+        <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
