b431e182ed8243345fef7474b76702d3196027a4
Humberto Fernández <humbertof44@gmail.com>
2024-05-05 00:53:22 -0400

    Update styles of “Profile” components

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,16 +5,26 @@ const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Cargando ...</div>;
+    return (
+      <div className="flex items-center justify-center mb-8">
+        <p className="text-gray-600">Cargando ...</p>
+      </div>
+    );
   }
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
+      <>
+        <div className="border border-gray-300 rounded-lg p-6 mb-4">
+          <div className="flex items-center justify-center mb-2">
+            <img className="w-20 h-20 rounded-full object-cover mr-4" src={user.picture} alt={user.name} />
+            <div>
+              <h2 className="text-2xl font-semibold text-gray-800">{user.name}</h2>
+              <p className="text-gray-600">{user.email}</p>
+            </div>
+          </div>
+        </div>
+      </>
     )
   );
 };
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import LogoutButton from '../components/Logout';
+import Profile from '../components/Profile';
 
 const ProfilePage = () => {
   return (
@@ -8,6 +9,7 @@ const ProfilePage = () => {
       <div className="max-w-3xl px-8 py-12 bg-white shadow-xl rounded-lg">
         <h2 className="text-4xl font-bold text-center text-gray-800 mb-6">Mi Perfil</h2>
         <p className="text-lg text-gray-700 mb-8 text-center">Aquí puedes ver y actualizar tu información personal.</p>
+        <Profile />
         <div className="flex justify-center">
           <Link to="/inicio" className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-600 transition duration-300 ease-in-out mr-2">
             Volver
